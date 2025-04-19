3e0a449ff7e2ac714e044fdad0d9f0c86d9c92e5
Samuel Paredes <samueleliasparedes.10@gmail.com>
2025-04-19 16:38:22 -0300

    add sqlite connection and backend services and repositories

--- README.md
+++ README.md
@@ -34,3 +34,14 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+
+
+
+## Migrate database
+```bash
+# Guarda los cambios realizados en los schemas
+npx generate
+# Actualiza la bd los cambios de lo schemas
+npx push
+# Obtiene los cambios realizados en la bd
+npm pull
--- components/pages/portfolio.tsx
+++ components/pages/portfolio.tsx
@@ -1,19 +1,21 @@
-import { dataPortfolio } from "@/data";
+// import { dataPortfolio } from "@/data";
 import Title from "../shared/title";
 import Image from "next/image";
 import Link from "next/link";
 import { buttonVariants } from "../ui/button";
+import getDataPortfolioService from "@/backend/Services";
 
-const Portfolio = () => {
+const Portfolio = async () => {
+  const dataPortfolio = await getDataPortfolioService.getAllDataPortfolioService();
   return (
     <div className="p-4 max-w-4xl md:py-24 mx-auto" id="portfolio">
       <Title title="Portfolio" subtitle="Trabajos recientes" />
       <div className="grid md:grid-cols-3 gap-14 mt-4">
         {dataPortfolio.map((data) => (
           <div key={data.id}>
-            <h3 className="text-xl mb-4">{data.title}</h3>
+            <h3 className="text-xl mb-4">{data.titlo}</h3>
             <Image
-              src={data.image}
+              src={data.imagen || "/placeholder-image.png"}
               alt="Image"
               width={300}
               height={300}
@@ -22,14 +24,14 @@ const Portfolio = () => {
             <div className="mt-5 flex gap-7">
               <Link
                 className={buttonVariants({ variant: "outline" })}
-                href={data.urlGithub}
+                href={data.urlGithub || "#!"}
                 target="_blank"
               >
                 GitHub
               </Link>
               <Link
                 className={buttonVariants()}
-                href={data.urlDemo}
+                href={data.urlDemo || "#!"}
                 target="_blank"
               >
                 Live demo
