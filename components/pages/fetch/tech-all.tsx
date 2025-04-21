a9846c46abfd80628f3701f00983733da001d4e9
Samuel Paredes <samueleliasparedes.10@gmail.com>
2025-04-21 17:24:35 -0300

    test

--- components/pages/fetch/tech-all.tsx
+++ components/pages/fetch/tech-all.tsx
@@ -21,7 +21,6 @@ export async function apiFetch<T = unknown, R = unknown>({
     url += `/${encodeURIComponent(String(id))}`;
   }
   url += `?tipo=${encodeURIComponent(resource)}`;
-  console.log(url)
   const isFormData = typeof FormData !== "undefined" && data instanceof FormData;
 
   const opts: RequestInit = {
