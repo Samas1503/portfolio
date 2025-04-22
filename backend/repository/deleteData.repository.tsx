5a4eb21f4c5c785f9bddf93b17c7ead34dc10bf8
Samuel Paredes <samueleliasparedes.10@gmail.com>
2025-04-22 02:13:32 -0300

    fix delet repository

--- backend/repository/deleteData.repository.tsx
+++ backend/repository/deleteData.repository.tsx
@@ -3,8 +3,8 @@ import { eq } from "drizzle-orm";
 import { SchemaKeys, schemas } from "../data/schemas";
 
 const deleteDataByIdRepository = async (id: number, table: SchemaKeys) => {
-  const data = await db.delete(schemas[table].schema).where(eq(schemas[table].schema.id, id));
-  return data;
+  await db.delete(schemas[table].schema).where(eq(schemas[table].schema.id, id));
+  return id;
 };
 
 const repository = {
